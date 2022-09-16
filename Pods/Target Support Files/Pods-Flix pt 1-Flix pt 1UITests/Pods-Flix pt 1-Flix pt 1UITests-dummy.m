12fc3bc5871a2100280d3143e7fa69473972e658
K3nnneth <K.bui2@yahoo.com>
2022-09-16 16:53:21 -0700

    added flix to repository

---
+++ Pods/Target Support Files/Pods-Flix pt 1-Flix pt 1UITests/Pods-Flix pt 1-Flix pt 1UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flix_pt_1_Flix_pt_1UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Flix_pt_1_Flix_pt_1UITests
+@end
---
+++ Pods/Target Support Files/Pods-Flix pt 1Tests/Pods-Flix pt 1Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flix_pt_1Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Flix_pt_1Tests
+@end
