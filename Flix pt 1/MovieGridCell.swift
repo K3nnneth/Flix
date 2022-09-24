d307c84bb899458fc5a19aa94e66e9eacc3fd292
K3nnneth <K.bui2@yahoo.com>
2022-09-23 20:05:07 -0700

    Pt 2

---
+++ Flix pt 1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix pt 1
+//
+//  Created by Kenny Bui on 9/23/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
